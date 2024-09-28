de1d09ec7d80d46cee5a5eec80644b130d6a5507
ceramica-cleopatra <96940168+ceramica-cleopatra@users.noreply.github.com>
2024-09-28 18:42:09 +0300

    Delete driverTracking directory

--- driverTracking/src/main/java/com/dms/driverTracking/service/driverBalance/DriverBalanceService.java
+++
@@ -1,7 +0,0 @@
-package com.dms.driverTracking.service.driverBalance;
-
-import com.dms.driverTracking.dto.IntermediateDTO;
-
-public interface DriverBalanceService {
-	public IntermediateDTO getDriverBalance(Long driverId);
-}
--- driverTracking/src/main/java/com/dms/driverTracking/service/ordr/OrdrService.java
+++
@@ -1,8 +0,0 @@
-package com.dms.driverTracking.service.ordr;
-
-import java.util.Date;
-import com.dms.driverTracking.dto.IntermediateDTO;
-
-public interface OrdrService {
-	IntermediateDTO findOrdrList(Integer pageNo, Integer pageSize, String sortBy,Date updateDate);
-}
